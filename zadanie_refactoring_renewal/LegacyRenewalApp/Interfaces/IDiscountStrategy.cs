a4e240fcffaf30316f55cb57cea1809b75d8ea6f
Igor Kozuchowski <s33212@pjwstk.edu.pl>
2026-04-14 20:55:10 +0200

    wyciąganie logiki zniżek i tworzenie interfejsu

---
+++ zadanie_refactoring_renewal/LegacyRenewalApp/Interfaces/IDiscountStrategy.cs
@@ -0,0 +1,6 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IDiscountStrategy
+{
+        decimal CalculateDiscount(Customer customer, SubscriptionPlan plan, int seatCount, decimal baseAmount);
+}
