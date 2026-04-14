c22fec98cca3986bd7ef9372f3127f4548f7933a
Igor Kozuchowski <s33212@pjwstk.edu.pl>
2026-04-14 21:03:36 +0200

    wydzielenie logiki w zaleznosci od czasu

---
+++ zadanie_refactoring_renewal/LegacyRenewalApp/Logics/LoyaltyDiscount.cs
@@ -0,0 +1,11 @@
+﻿namespace LegacyRenewalApp;
+
+public class LoyaltyDiscount : IDiscountStrategy
+{
+    public (decimal Amount, string Note) Calculate(Customer customer, SubscriptionPlan plan, int seatCount, decimal baseAmount)
+    {
+        if (customer.YearsWithCompany >= 5) return (baseAmount * 0.07m, "long-term loyalty discount; ");
+        if (customer.YearsWithCompany >= 2) return (baseAmount * 0.03m, "basic loyalty discount; ");
+        return (0, string.Empty);
+    }
+}
